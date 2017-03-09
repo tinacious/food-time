07c8577ac08df441b774c5b86c211db561fa7cf7
Tina Holly <tinacious.design@gmail.com>
2017-03-08 19:55:41 -0500

    Delete comments, reorder code: function declared first then attach it on document ready; alert user when option not chosen.

--- order.js
+++ order.js
@@ -1,29 +1,6 @@
-/*
-
-  - hide all js-food-options
-
-  - user chooses a food option: sushi, burger, salad
-  
-  - add an event listener based on the selection they make
-    - ??? how do we add this?
-    - what does it do?
-        - we want the next option to show up
-          - what is my option?
-
-          - how do i make it show up?
-
-
-IINSTRUCTIONS
-
-- attach an event listener that logs to the console
-
-*/
-
-
-
-$(document).ready(initFoodMenu);
-
-
+/**
+ * Initializes the food menu
+ */
 function initFoodMenu() {
   // DOM elements
   var $burgerToppings = $('#burger-toppings');
@@ -63,7 +40,10 @@ function initFoodMenu() {
         break;
 
       default:
-        console.log('you have to choose an option!!!')
+        alert('you have to choose an option!!!');
     }
   }
 }
+
+
+$(document).ready(initFoodMenu);
